daf9a1b70e29613769f85fcd7b8e38febedc959b
funhunt58-SeaBass <funhunt58@gmail.com>
2026-03-24 19:49:18 -0400

    Revert "Script merge"
    This reverts commit 31469a9c75d46074e5b112f05c768580004fb558, reversing
    changes made to cfe51029ed2aaf88fe969ebc9463a72f324c691a.

--- Assets/Script_folder/ModsPickUps.cs
+++ Assets/Script_folder/ModsPickUps.cs
@@ -1,65 +1,16 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Threading;
 
 public class ModifierPickup : MonoBehaviour
 {
     [SerializeField] private StatsModifierSO modifierTemplate;
-    public string Name;
-    public int ItemId;
-    public Sprite Image;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
+        if (!other.TryGetComponent(out StatManager stats)) return;
 
+        var rolled = ModifierRoller.Roll(modifierTemplate);
+        stats.AddRolledModifier(rolled);
 
-            if (other.gameObject.GetComponent<StatsManager>() == null)
-            {
-                Debug.Log("StatManger == null");
-            }
-            //return;/
-
-            var rolled = ModifierRoller.Roll(modifierTemplate);
-            other.gameObject.GetComponent<StatsManager>().AddRolledModifier(rolled);
-            var stat = other.gameObject.GetComponent<StatsManager>();
-            Debug.Log("Stat = " + stat.gameObject);
-
-            Inventory inventory = other.gameObject.GetComponent<Inventory>();
-            Inventory_Item newItem = new Inventory_Item();
-            newItem.Name = Name;
-            newItem.ItemID = ItemId;
-            newItem.Image = Image;
-            
-            inventory.Inventory_Slots.Add(newItem);
-            /* bool HasItem = false;
-            if (inventory.Inventory_Slots.Count > 0)
-            {
-                for (int i = 0; i < inventory.Inventory_Slots.Count; i++)
-                {
-                    if (inventory.Inventory_Slots[i].ItemID == newItem.ItemID)
-                    {
-                        inventory.Inventory_Slots[i].Count++; 
-                        HasItem = true;
-                    }
-                    
-                }
-                if (HasItem == false)
-                {
-
-                    inventory.Inventory_Slots.Add(newItem);
-                }
-
-            }
-            else
-            {
-                inventory.Inventory_Slots.Add(newItem);
-
-            }*/
-
-
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
