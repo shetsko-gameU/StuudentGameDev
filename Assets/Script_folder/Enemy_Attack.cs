270c904e04cd8935492c374c50c9cd8bd5a6ff7c
Jake Simon <speedshoes29@optonline.net>
2026-04-16 21:59:41 -0400

    Update Enemy Attack
    Finished draft for enemy to attack.  Some errors need to be fixed

--- Assets/Script_folder/Enemy_Attack.cs
+++ Assets/Script_folder/Enemy_Attack.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Enemy_Attack : Enemy_State
 {
+    Transform playerTransform;
+    float timer;
+    float timeBetweenAttacks = 1f; // Example attack cooldown
+    float exitTimer;
+    float timeToExitAfterAttack = 2f; // Time to exit attack state after
+    float distanceToCountExit = 3f; // Distance to player to start exit timer
     public Enemy_Attack(Enemy_Base enemy, Enemy_State_Machine enemyStateMachine)
     {
         this.enemy = enemy;
         this.enemyStateMachine = enemyStateMachine;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     public override void EnterState()
@@ -15,7 +22,26 @@ public class Enemy_Attack : Enemy_State
 
     public override void FrameUpdate()
     {
-        // per-frame attack logic
+        enemy.moveState(Vector3.zero); // Stop movement during attack
+        if(timer >= timeBetweenAttacks)
+        {
+            OnAttack();
+            timer = 0;
+            //Vector2 dir = (playerTransform.position - enemy.transform.position).normalized;
+        }
+        if(exitTimer >= timeToExitAfterAttack)
+        {
+            enemy.StateMachine.ChangeState(enemy.moveState);
+        }
+        else if(Vector3.Distance(enemy.transform.position, playerTransform.position) > distanceToCountExit)
+        {
+            exitTimer += Time.deltaTime;
+        }
+        else
+        {
+            exitTimer = 0; // reset exit timer if player is close again
+        }
+        timer += Time.deltaTime;
     }
 
     public override void PhysicsUpdate()
