b045d7ac3f882474535f8651248515f68bbaf5ac
gamygamer838-coder <gamygamer838@gmail.com>
2026-06-23 23:24:44 -0400

    added shopkeeper
    added shopkeeper model

---
+++ Assets/Script_folder/vfx/CutOutManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CutOutManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetObject;
+
+    [SerializeField]
+    private LayerMask obstructitons;
+
+    private Camera mainCamera;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
