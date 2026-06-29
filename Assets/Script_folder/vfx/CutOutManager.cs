ac92ae61d9667534bba519e48aa9261a32f50061
gamygamer838-coder <gamygamer838@gmail.com>
2026-06-29 12:57:49 -0400

    finished code for view cutout
    finished code for shader to cutout hole from obstructions of the player and camera

--- Assets/Script_folder/vfx/CutOutManager.cs
+++ Assets/Script_folder/vfx/CutOutManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using System.Collections;
 public class CutOutManager : MonoBehaviour
 {
     [SerializeField]
@@ -10,16 +11,31 @@ public class CutOutManager : MonoBehaviour
 
     private Camera mainCamera;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        mainCamera = GetComponent<Camera>();
     }
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
+        cutoutPos.y /= (Screen.width / Screen.height);
+
+        Vector3 offset = targetObject.position - transform.position;
+        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, obstructitons);
+        for (int i = 0; i < hitObjects.Length; i++)
+        {
+            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+
+            for (int m = 0; m < materials.Length; m++)
+            {
+                materials[m].SetVector("cutOutPos", cutoutPos);
+                materials[m].SetFloat("size", 0.1f);
+                materials[m].SetFloat("fallOffSize", 0.05f);
+            }
+        }
     }
 }
