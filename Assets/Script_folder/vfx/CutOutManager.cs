9beb288dd515141f4154b84f00ad093242fc3da2
gamygamer838-coder <gamygamer838@gmail.com>
2026-07-01 16:09:25 -0400

    tweeked cutout shader

--- Assets/Script_folder/vfx/CutOutManager.cs
+++ Assets/Script_folder/vfx/CutOutManager.cs
@@ -10,10 +10,11 @@ public class CutOutManager : MonoBehaviour
     private LayerMask obstructitons;
 
     private Camera mainCamera;
-
+    private RaycastHit[] raycastHits;
     private void Awake()
     {
         mainCamera = GetComponent<Camera>();
+        raycastHits = new RaycastHit[0];
     }
 
    
@@ -21,21 +22,43 @@ public class CutOutManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < raycastHits.Length; i++)
+        {
+            Material[] materials = raycastHits[i].transform.GetComponent<Renderer>().materials;
+
+            for (int m = 0; m < materials.Length; m++)
+            {
+                materials[m].SetVector("cutOutPos", Vector4.zero);
+                materials[m].SetFloat("size", 0f);
+                materials[m].SetFloat("fallOffSize", 0f);
+            }
+            
+
+        }
         Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
         cutoutPos.y /= (Screen.width / Screen.height);
 
         Vector3 offset = targetObject.position - transform.position;
-        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, obstructitons);
-        for (int i = 0; i < hitObjects.Length; i++)
+        //raycastHits = Physics.RaycastAll(transform.position, offset, offset.magnitude, obstructitons);
+        raycastHits = Physics.SphereCastAll(transform.position,1.5f, offset, offset.magnitude - 1.5f, obstructitons);
+        for (int i = 0; i < raycastHits.Length; i++)
         {
-            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+            Material[] materials = raycastHits[i].transform.GetComponent<Renderer>().materials;
 
             for (int m = 0; m < materials.Length; m++)
             {
                 materials[m].SetVector("cutOutPos", cutoutPos);
-                materials[m].SetFloat("size", 0.1f);
+                materials[m].SetFloat("size", 0.15f);
                 materials[m].SetFloat("fallOffSize", 0.05f);
             }
+            Debug.Log(raycastHits[i].transform.gameObject.name);
+            
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 offset = targetObject.position - transform.position;
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, offset);
     }
 }
